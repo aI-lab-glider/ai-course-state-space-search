9decf98539de5529cd250a46e99efb5c8a8d1ec3
Kollbi <lkollbek@gmail.com>
2021-09-19 12:23:28 +0200

    Merged main.py beacuse of conflict while pulling to local.

--- src/main.py
+++ src/main.py
@@ -1,10 +1,7 @@
-
 from problems.n_puzzle_problem import NPuzzleProblem
-from solvers.bfs import BFS
-
+from solvers.breadth_first import BFS
 
 if __name__ == '__main__':
     p = NPuzzleProblem
     s = BFS
     
-
