7e2abc2c29cab814d5bc27b9d60ff6a679a2b501
czyjtu <maciejczyjtu@gmail.com>
2021-09-19 12:38:18 +0200

    Adde useful methods to node class, and fixed tree.expand

--- src/tree/node.py
+++ src/tree/node.py
@@ -13,6 +13,22 @@ class Node:
     def __lt__(self, other):
         return self.cost < other.cost
 
+    
+    def __str__(self):
+        return str(self.state)
+
+    
+    def __repr__(self):
+        return f"<{str(self.parent)} --{self.action}--> {str(self.state)}. cost: {self.cost}>"
+
+
+    def path(self):
+        node, path = self, []
+        while node:
+            path.append(node)
+            node = node.parent
+        return path[::-1]
+
 
     def add_child(self, child):
         self.children.add(child)
