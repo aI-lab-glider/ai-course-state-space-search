391fe070fe50ded51a9b271cbe9d06162f67ce8b
mslazynski <mateusz.slazynski@gmail.cm>
2021-11-03 21:41:45 +0100

    Small improvements in the search tree subscribing

--- src/tree/tree.py
+++ src/tree/tree.py
@@ -1,25 +1,41 @@
+from abc import ABC, abstractmethod
+from enum import Enum, auto
+from typing import List
 from tree import Node
+
+
+class NodeEvent(Enum):
+    Closed = auto()
+    Opened = auto()
+
+
+class NodeEventSubscriber(ABC):
+
+    @abstractmethod
+    def got_event(self, node: Node, event: NodeEvent) -> None:
+        pass
 
 
 class Tree:
     def __init__(self, root):
         self.root = root
-        self.subscribers = []
+        self.subscribers : List[NodeEventSubscriber] = []
 
 
-    def subsribe(self, subscriber):
+    def subscribe(self, subscriber: NodeEventSubscriber):
         """Add new subscriber to the list"""
         self.subscribers.append(subscriber)
 
 
-    def _notify(self, node):
+    def _notify(self, node: Node, event: NodeEvent):
         """Notify subscriber about new node event"""
         for subscriber in self.subscribers:
-            subscriber.update(node)
+            subscriber.got_event(node, event)
 
 
     def expand(self, problem, node):
         """Generator over child nodes"""
+        self._notify(node, NodeEvent.Closed)
         for action in problem.actions(node.state):
             child_state = problem.take_action(node.state, action)
             child_node = Node(
@@ -28,8 +44,8 @@ class Tree:
                 cost=node.cost + problem.action_cost(node.state, action, child_state),
                 action=action
                 )
-            node.add_child(child_node)
-            self._notify(child_node)
+
+            self._notify(child_node, NodeEvent.Opened)
             yield child_node
 
 
