259d22ad0a6a5994a3eb4754a3e47edf5248d6d5
Bohdan <qwebeck5@gmail.com>
2021-11-14 23:45:16 +0100

    Fixed version of bidirectional astar

--- solvers/generic/bidirectional_search.py
+++ solvers/generic/bidirectional_search.py
@@ -14,36 +14,61 @@ class SearchProcess:
         self.start_state, self.goal_state = start_state, goal_state
         self.problem = problem
         self.eval_fun = eval_fun
-        self.tree = Tree(Node(start_state))
-        self._visited = {self.start_state: self.tree.root.cost}
-        self.frontier = PriorityQueue(eval_fun, [self.tree.root])
         self.heuristic = heuristic
-        self.solution_node = self.tree.root
+        self.tree = Tree(Node(start_state))
+        self._visited = {self.tree.root.state: self.tree.root.cost}
+        self.frontier = PriorityQueue(eval_fun, [self.tree.root])
+        self.meeting_point: Optional[Node] = None
 
     @property
-    def path(self) -> List[Node]:
-        return self.solution_node.path()
+    def cost_estimate(self):
+        return float('inf') if not self.meeting_point else self.eval_fun(self.meeting_point)
 
-    def expand_frontier(self, upper_bound_cost: float, opposite_process: 'SearchProcess'):
+    def expand_frontier(self, upper_bound_cost: float, other_process: 'SearchProcess'):
         """
-        Solves tree in one direction.
+        Discovers nodes
         """
-        candidate_found = True
-        while candidate_found:
-            candidates = self._find_candidates(upper_bound_cost)
-            candidate = self._select_candidate(candidates, upper_bound_cost,
-                                               opposite_process)
-            candidate_found = candidate is not None
-            if candidate is not None:
-                candidate_found = True
-                if not opposite_process.has_visited_state(candidate.state):
-                    for node in self.tree.expand(self.problem, candidate):
-                        if self.state_cost(node.state) > node.cost:
-                            self._visited[node.state] = node.cost
-                            self.solution_node = node
-                            self.frontier.push(node)
-                            upper_bound_cost = min(upper_bound_cost, self.state_cost(node.state)
-                                                   + opposite_process.state_cost(node.state))
+        candidates = self._find_candidates(upper_bound_cost)
+        candidate = self._select_candidate(
+            candidates, upper_bound_cost, other_process)
+        if candidate is not None:
+            if not other_process.has_visited_state(candidate.state):
+                for node in self.tree.expand(self.problem, candidate):
+                    if self.state_cost(node.state) > node.cost:
+                        new_upper_bound_cost = self._update_stats(
+                            node, other_process)
+                        if new_upper_bound_cost < upper_bound_cost:
+                            upper_bound_cost = new_upper_bound_cost
+                            self.meeting_point = node
+        return upper_bound_cost
+
+    def _find_candidates(self, upper_bound_cost) -> PriorityQueue:
+        candidates = PriorityQueue(self.eval_fun)
+        while not self.frontier.is_empty():
+            candidate = self.frontier.pop()
+            if self.eval_fun(candidate) - self.heuristic(self.goal_state) < upper_bound_cost:
+                candidates.push(candidate)
+        return candidates
+
+    def _select_candidate(self, candidates: PriorityQueue[Node], upper_bound_cost: float,
+                          other_process: 'SearchProcess') -> Optional[Node]:
+        candidate = None
+        while candidates and not candidate:
+            candidate = candidates.pop()
+            cost_through_state = self.state_cost(
+                candidate.state) + other_process.cost_estimate + other_process.heuristic(candidate.state)
+            if not other_process.has_visited_state(candidate.state) and cost_through_state >= upper_bound_cost:
+                continue
+            else:
+                break
+        return candidate
+
+    def _update_stats(self, node: Node, other_process: 'SearchProcess') -> float:
+        self._visited[node.state] = node.cost
+        self.meeting_point = node
+        self.frontier.push(node)
+        upper_bound_cost = self.state_cost(
+            node.state) + other_process.state_cost(node.state)
         return upper_bound_cost
 
     def state_cost(self, state: State):
@@ -51,28 +76,6 @@ class SearchProcess:
 
     def has_visited_state(self, state: State):
         return state in self._visited
-
-    def _find_candidates(self, upper_bound_cost) -> PriorityQueue:
-        candidates = PriorityQueue(self.eval_fun)
-        while not self.frontier.is_empty():
-            candidate = self.frontier.pop()
-            for node in self.tree.expand(self.problem, candidate):
-                if self.eval_fun(node) - self.heuristic(self.goal_state) < upper_bound_cost:
-                    candidates.push(node)
-        return candidates
-
-    def _select_candidate(self, candidates: PriorityQueue[Node], upper_bound_cost: float,
-                          opposite_process: 'SearchProcess') -> Optional[Node]:
-        candidate = None
-        while candidates and not candidate:
-            candidate = candidates.pop()
-            cost_through_state = self.state_cost(
-                candidate.state) + opposite_process.state_cost(candidate.state)
-            if not opposite_process.has_visited_state(candidate.state) and cost_through_state >= upper_bound_cost:
-                continue
-            else:
-                break
-        return candidate
 
     def can_expand_frontier(self):
         return not self.frontier.is_empty()
@@ -83,24 +86,27 @@ class BidirectionalSearch:
     def __init__(self, problem: Problem[State, Any], eval_fun: Callable[[Node], float],
                  heuristic: Heuristic = NoHeuristic()):
         self.problem = problem
-        self.forward_process = SearchProcess(problem,
-                                             problem.initial, problem.goal,
+        self.forward_search = SearchProcess(problem,
+                                            problem.initial, problem.goal,
+                                            eval_fun, heuristic)
+        self.backward_search = SearchProcess(problem,
+                                             problem.goal, problem.initial,
                                              eval_fun, heuristic)
-        self.backward_process = SearchProcess(problem,
-                                              problem.goal, problem.initial,
-                                              eval_fun, heuristic)
 
-    @property
+    @ property
     def search_tree(self):
-        return self.forward_process.tree
+        return self.forward_search.tree
 
     def solve(self):
         upper_bound_cost = float('inf')
-        while self.forward_process.can_expand_frontier() and self.backward_process.can_expand_frontier():
-            upper_bound_cost = self.forward_process.expand_frontier(
-                upper_bound_cost, self.backward_process)
-            upper_bound_cost = self.backward_process.expand_frontier(
-                upper_bound_cost, self.forward_process)
+        while self.forward_search.can_expand_frontier() and self.backward_search.can_expand_frontier():
+            upper_bound_cost = self.forward_search.expand_frontier(
+                upper_bound_cost, self.backward_search)
+            upper_bound_cost = self.backward_search.expand_frontier(
+                upper_bound_cost, self.forward_search)
 
-        self.backward_process.path[0].parent = self.forward_process.path[-1]
-        return self.backward_process.path[-1]
+        if self.backward_search.meeting_point and self.forward_search.meeting_point:
+            solution_node = self.backward_search.meeting_point.path()[0]
+            self.backward_search.meeting_point.parent.parent = self.forward_search.meeting_point
+            return solution_node
+        return None
